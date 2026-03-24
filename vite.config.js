697d656dc63ee3cf2401daedd1f6876906ccda8f
aarturolopez <aalopez@bancomercantil.com>
2026-03-24 13:22:40 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cinegraphixx/",  // <--- ADD THIS LINE (Must match your repo name)
+  base: "/proyectofinal/",  // <--- ADD THIS LINE (Must match your repo name)
 })
